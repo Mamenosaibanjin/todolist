0a9d32da39a103a9fa7de86a315a159b609aa767
Mamenosaibanjin <92902099+Mamenosaibanjin@users.noreply.github.com>
2021-10-21 07:30:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,28 @@ include ("applications_de.php");
 
 <body>
 
+<?php 
+
+if (isset($_GET['form_todo_submit'])) {
+
+	$todoName = $_GET['form_todo_name']; 
+	$todoDescription = $_GET['form_todo_description']; 
+	$todoProcessing = $_GET['form_todo_processing']; 
+	$todoCreated = date("Y-m-d H:i:s"); 
+	$todoUpdated = date("Y-m-d H:i:s");
+
+	$queryNewTodo = "INSERT INTO todo_list 
+						(name, description, processing_date, created_at, updated_at) VALUES 
+						('" . $todoName . "', '" . $todoDescription . "', '" . $todoProcessing . "', '" . $todoCreated . "', '" . $todoUpdated . "')";
+
+	$todo = mysqli_query($dbLink, $queryNewTodo); 
+
+	echo "ToDo erfolgreich gespeichert!<br><br>";
+}
+
+?>
+
+
 <?php
 $queryTodo = "SELECT * FROM todo_list";
 $todo = mysqli_query($dbLink, $queryTodo); 
@@ -26,7 +48,7 @@ $todo = mysqli_query($dbLink, $queryTodo);
 		<th>#</th>
 		<th>Titel</th>
 		<th>Beschreibung</th>
-		<th>Processing</th>
+		<th>Bearbeitungsdatum</th>
 		<th>Erstellt</th>
 		<th>Zuletzt bearbeitet</th>
 	</tr>
@@ -53,6 +75,47 @@ $todo = mysqli_query($dbLink, $queryTodo);
 		</tr>
 	<?php } ?>
 </table>
+
+<br><br>
+
+<form ACTION="todolist.php" METHOD="GET">
+	<table cellpadding="5" cellspacing="0" border="0">
+		<tr>
+			<th>
+				Name der ToDo
+			</th>
+			<td>
+				<input type="text" size="50" maxlength="255" name="form_todo_name">
+			</td>
+		</tr>
+
+		<tr>
+			<th>
+				Beschreibung
+			</th>
+			<td>
+				<textarea rows="3" cols="20" name="form_todo_description"></textarea>
+			</td>
+		</tr>
+
+		<tr>
+			<th>
+				Bearbeitungsdatum
+			</th>
+			<td>
+				<input type="date" name="form_todo_processing">
+			</td>
+		</tr>
+
+		<tr>
+			<th>
+				&nbsp;
+			</th>
+			<td>
+				<input type="submit" name="form_todo_submit" value="Formular absenden">
+			</td>
+		</tr>
+	</table>
 
 </body>
 </html>
