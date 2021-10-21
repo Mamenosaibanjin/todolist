18a46699574365492c74e89aaa6bb7406015059f
Mamenosaibanjin <92902099+Mamenosaibanjin@users.noreply.github.com>
2021-10-21 06:43:09 +0200

    Create applications_de.php

---
+++ applications_de.php
@@ -0,0 +1,8 @@
+<?php
+define("__WEBSITE__", "localhost/todo");
+
+define("__CHARSET__", "<meta http-equiv=\"content-type\" content=\"MIME-Typ; charset=ISO-8859-15\" />");
+
+define("__TITLE__", "ToDo-Liste");
+define("__SUBTITLE__", "");
+?>
