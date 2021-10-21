65fb1e3faa0192bb221be343c67769fc664b2fd6
Mamenosaibanjin <92902099+Mamenosaibanjin@users.noreply.github.com>
2021-10-21 06:50:40 +0200

    Create index.php
    Grundgerüst

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+include ("db.php");
+include ("applications_de.php");
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php echo __CHARSET__ ?>
+<title><?php echo __TITLE__ ?></title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+<script type="text/javascript" src="js/ajax.js"></script>
+<script type="text/javascript" src="js/libs.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+
+</body>
+</html>
